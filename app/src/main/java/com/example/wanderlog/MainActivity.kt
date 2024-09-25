0b066f614bdf4042f2492bbf8289f5a00b8b9806
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-09-25 00:01:41 -0500

    Update Activity Main

--- app/src/main/java/com/example/wanderlog/MainActivity.kt
+++ app/src/main/java/com/example/wanderlog/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.wanderlog
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +18,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Configurar el toolbar
+        val toolbar = findViewById<Toolbar>(R.id.myToolbar)
+        setSupportActionBar(toolbar)
+
+        // Remover el título del Toolbar si no es necesario
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Encontrar el botón por su ID
+        val button = findViewById<ImageButton>(R.id.ibPackages)
+
+        // Configurar el click listener del botón
+        button.setOnClickListener {
+            // Crear un Intent para ir a la segunda actividad
+            val intent = Intent(this, Search::class.java)
+            startActivity(intent) // Iniciar la segunda actividad
         }
     }
 }
