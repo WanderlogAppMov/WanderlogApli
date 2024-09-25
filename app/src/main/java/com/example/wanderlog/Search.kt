f0c18632d7629380de8b83fb92227661fc029414
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-09-25 00:01:27 -0500

    Create Activity Search

---
+++ app/src/main/java/com/example/wanderlog/Search.kt
@@ -0,0 +1,45 @@
+package com.example.wanderlog
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_search)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configurar el toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Desactivar el título si no lo necesitas
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Configurar el botón en el Toolbar
+        val toolbarButton = findViewById<ImageButton>(R.id.toolbarButton)
+        val btSearch = findViewById<Button>(R.id.btSearch)
+
+        btSearch.setOnClickListener {
+            val intent = Intent(this, Results::class.java)
+            startActivity(intent)
+        }
+
+        toolbarButton.setOnClickListener {
+            finish()
+        }
+
+    }
+}
